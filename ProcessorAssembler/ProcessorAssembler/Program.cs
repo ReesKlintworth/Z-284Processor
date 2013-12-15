730de0688cc3acafb18ad9c89195b50161b79cc3
dnordgren <dnordgren.nordgren@gmail.com>
2013-12-15 14:25:44 -0600

    Assembler is more robust. Still need to check for syntax errors

--- ProcessorAssembler/ProcessorAssembler/Program.cs
+++ ProcessorAssembler/ProcessorAssembler/Program.cs
@@ -12,6 +12,7 @@ namespace ProcessorAssembler
 	{
 		static void Main(string[] args)
 		{
+			#region hashtables
 			Hashtable opCodes = new Hashtable();
 			opCodes["and"] = "0000";
 			opCodes["add"] = "0000";
@@ -42,103 +43,147 @@ namespace ProcessorAssembler
 			aluCodes["sub"] = "101";
 			aluCodes["sll"] = "110";
 			aluCodes["srl"] = "111";
+			#endregion
+			Console.WriteLine("Specify input .s file.");
+			string filename = Console.ReadLine();
 
-			string[] lines = File.ReadAllLines(@"C:\Users\Derek Nordgren\Documents\GitHub\284Processor\SampleAssemblyProgram.s");
-
-			List<string> outputLines = new List<string>();
-
-			outputLines.Add("WIDTH = 16");
-			outputLines.Add("DEPTH = " + lines.Length);
-			outputLines.Add("ADDRESS_RADIX = DEC;");
-			outputLines.Add("DATA_RADIX = BIN;");
-			outputLines.Add("\nCONTENT");
-			outputLines.Add("\tBEGIN");
-
-			Hashtable methods = new Hashtable();
-
-			// Sets method line positions
-			for (int currentLineIndex = 0; currentLineIndex < lines.Length; currentLineIndex++)
+			if (!filename.EndsWith(".s"))
 			{
-				string currentLine = lines[currentLineIndex];
-				if (!currentLine.StartsWith("\t"))
-				{
-					string methodName = currentLine.Split(':')[0];
-					methods[methodName] = (Int32)currentLineIndex;
-
-				}
+				Console.WriteLine("Whoops! Selected file is not .s. Press Return to exit.");
+				Console.ReadLine();
+				return;
 			}
-
-			for (int i = 0; i < lines.Length; i++)
+			try
 			{
-				string outputLine = "\t" + i + "\t:\t";
+				string[] lines = File.ReadAllLines(filename);
 
-				string currentLine = lines[i];
 
-				string[] splitByTab = currentLine.Split('\t');
+				List<string> outputLines = new List<string>();
 
-				string[] components = splitByTab[1].Split(' ');
+				outputLines.Add("WIDTH = 16");
+				outputLines.Add("DEPTH = " + lines.Length);
+				outputLines.Add("ADDRESS_RADIX = DEC;");
+				outputLines.Add("DATA_RADIX = BIN;");
+				outputLines.Add("\nCONTENT");
+				outputLines.Add("\tBEGIN");
 
-				string op = opCodes[components[0]].ToString();
-				
-				outputLine += op + " ";
+				Hashtable methods = new Hashtable();
 
-				string[] registers = components[1].Split(',');
-
-				int rTypeCounter = 0;
-				foreach (string reg in registers)
+				// Sets method line positions
+				for (int currentLineIndex = 0; currentLineIndex < lines.Length; currentLineIndex++)
 				{
-					int convertedReg = 0;
-					// getting register
-					if (reg.StartsWith("$"))
+					string currentLine = lines[currentLineIndex];
+					if (!currentLine.StartsWith("\t"))
 					{
-						int register = Int32.Parse(reg[1].ToString());
-						string binReg = Convert.ToString(register, 2);
-						binReg = binReg.PadLeft(3, '0');
-						outputLine += binReg + " ";
-						rTypeCounter++;
+						string methodName = currentLine.Split(':')[0];
+						methods[methodName] = (Int32)currentLineIndex;
 					}
-					
-					// getting immediate
-					else if (Int32.TryParse(reg, out convertedReg))
-					{
-						string immBin = Convert.ToString(convertedReg, 2);
-						immBin = immBin.PadLeft(6, '0');
-						outputLine += immBin + " ";
-					}
+				}
 
-					else if (reg.Contains("("))
-					{
-						string[] pieces = reg.Split('(');
-						string iTypeImm = Convert.ToString(Int32.Parse(pieces[0]), 2);
-						iTypeImm = iTypeImm.PadLeft(6, '0');
-						string iTypeReg = pieces[1][1].ToString();
-						iTypeReg = iTypeReg.PadLeft(3, '0');
-						outputLine += iTypeReg + " ";
-						outputLine += iTypeImm + " ";
-					}
+				for (int i = 0; i < lines.Length; i++)
+				{
+					string outputLine = "\t" + i + "\t:\t";
 
-					else
+					string currentLine = lines[i];
+
+					string[] splitByTab = currentLine.Split('\t');
+
+					string[] components = splitByTab[1].Split(' ');
+
+					string op = opCodes[components[0]].ToString();
+
+					outputLine += op;
+
+					string[] registers = components[1].Split(',');
+
+					int rTypeCounter = 0;
+					foreach (string reg in registers)
 					{
-						string method = reg;
-						int methodNum = Convert.ToInt32(methods[method]);
-						string methodNumStr = Convert.ToString(methodNum, 2);
-						methodNumStr = methodNumStr.PadLeft(6, '0');
-						if (components[0] == "j")
+						#region parse arguments
+						int convertedReg = 0;
+						// getting register
+						if (reg.StartsWith("$"))
 						{
-							methodNumStr = methodNumStr.PadLeft(12, '0'); 
+							int register = Int32.Parse(reg[1].ToString());
+							string binReg = Convert.ToString(register, 2);
+							binReg = binReg.PadLeft(3, '0');
+							outputLine += binReg;
+							rTypeCounter++;
 						}
-						outputLine += methodNumStr + " ";
+
+						// getting immediate
+						else if (Int32.TryParse(reg, out convertedReg))
+						{
+							string immBin = Convert.ToString(convertedReg, 2);
+							immBin = immBin.PadLeft(6, '0');
+							outputLine += immBin;
+						}
+
+						else if (reg.Contains("("))
+						{
+							string[] pieces = reg.Split('(');
+							string iTypeImm = Convert.ToString(Int32.Parse(pieces[0]), 2);
+							iTypeImm = iTypeImm.PadLeft(6, '0');
+							string iTypeReg = pieces[1][1].ToString();
+							iTypeReg = iTypeReg.PadLeft(3, '0');
+							outputLine += iTypeReg;
+							outputLine += iTypeImm;
+						}
+
+						else
+						{
+							string method = reg;
+							int methodNum = Convert.ToInt32(methods[method]);
+							string methodNumStr = Convert.ToString(methodNum, 2);
+							methodNumStr = methodNumStr.PadLeft(6, '0');
+							if (components[0] == "j")
+							{
+								methodNumStr = methodNumStr.PadLeft(12, '0');
+							}
+							outputLine += methodNumStr;
+						}
+						#endregion
 					}
+
+					if (rTypeCounter == 3)
+					{
+						outputLine += aluCodes[components[0]];
+					}
+					outputLines.Add(outputLine);
 				}
 
-				if (rTypeCounter == 3)
-				{
-					outputLine += aluCodes[components[0]];
-				}
-				outputLines.Add(outputLine);
+				Console.WriteLine("Assembly successful. Specify output location.");
+				string outputFilename = Console.ReadLine();
+
+				File.WriteAllLines(outputFilename, outputLines.ToArray());
+				Console.WriteLine("Output file written successfully. Press Return to exit.");
+				Console.ReadLine();
+				return;
 			}
-
-			File.WriteAllLines(@"C:\Users\Derek Nordgren\Documents\GitHub\284Processor\FirstMif.mif", outputLines.ToArray());
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("File not found. Press Return to exit.");
+				Console.ReadLine();
+				return;
+			}
+			catch (NullReferenceException)
+			{
+				Console.WriteLine("File not formatted correctly. Press Return to exit.");
+				Console.ReadLine();
+				return;
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine("File not formatted correctly. Press Return to exit.");
+				Console.ReadLine();
+				return;
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Output file not able to be written. Press Return to exit.");
+				Console.ReadLine();
+				return;
+			}
 		}
 	}
 }
---
+++ ProcessorAssembler/ProcessorAssembler/bin/Debug/test.s
@@ -0,0 +1,14 @@
+	addi $1,$0,1
+init:	add $7,$0,$0
+loop:	sw $7,0($0)
+	lw $6,1($0)
+	lw $4,2($0)
+	beq $4,$1,addone
+	beq $3,$1,init
+	j loop
+addone:	lw $2,0($0)
+	add $7,$7,$6
+	sw $7,0($0)
+wait:	lw $2,1($0)
+	beq $3,$1,wait
+	j loop
