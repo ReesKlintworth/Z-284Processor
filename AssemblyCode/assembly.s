6a4e114ca781b6bb55384984386aef8ea27db048
Derek Tiggelaar <dtiggelaar@cmsdm.com>
2013-12-18 23:59:29 -0600

    Actually, this is the assembly code w/o comments

--- AssemblyCode/assembly.s
+++ AssemblyCode/assembly.s
@@ -1,39 +1,27 @@
-
-lw $1,0($0)		;; get the first input
-;; prepare the board for a second input
-lw $2,0($0)		;; get the second input
-
-
-addi $7,$0,1	;; move 1 into $7 --> $7 is the shifting register (always 1)
-addi $3,$0,1	;; move 1 into $3 --> $3 is the masking register
-addi $5,$0,0	;; move 1 into $5 --> $5 is the index register
+lw $1,0($0)
+lw $2,0($0)	
+addi $7,$0,1
+addi $3,$0,1	
+addi $5,$0,0	
 
 loop:
-
-	and $4,$2,$3		;; get the ith bit of the second number and store into $4 --> temp value register
-	beq $0,$4,mask		;; skip to 'mask' if the ith bit of the second number isnt 1
-
-	sll $4,$1,$4		;; shift the first number left by the masked number and store into $4
-	add $6,$4,$6		;; store running total into $6 --> register to return
-	
-	;; detect overflow and jump to overflow method
+	and $4,$2,$3		
+	beq $0,$4,mask
+	sll $4,$1,$4		
+	add $6,$4,$6		
 	
 mask:
-
-	sll $3,$3,$7		;; shift the mask down for the next bit
-
-	add $5,$5,$7		;; iterate register index
-
-	mov $4,8			;; store 8 into register $4 for next operation
-
-	beq $8,$5,end		;; jumps to the end of the program
-	jmp loop
-
+	sll $3,$3,$7		
+	add $5,$5,$7		
+	sub $4,$4,$4
+	addi $4,$4,8			
+	beq $8,$5,end		
+	j loop
 
 overflow:
 	beq 1($0),$0,loop
 	sw $0,1($0)
-	jmp mask
+	j mask
 
 end:
-	add $4,$0,$4		;; does nothing important, just at the need something at the end
+	add $4,$0,$4
